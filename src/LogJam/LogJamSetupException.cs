42183d391f6fb67280d493408764c69952c3a75d
John Crim <john.crim@gmail.com>
2015-02-25 22:48:29 -0800

    * Added IBufferingLogWriter and BufferingLogWriter - need to revisit * Added test: BackgroundMultiLogWriterToText * Debugged use of starting IMultiLogWriter thanks to the test
    TODO:
    * Add ILogJamComponent, possibly replacing IStartable and making the component tree explorable?
    ** Remove separate SafeStart for collections and individual components.
    * Then, remove SafeStart, SafeStop, and SafeDispose for IMultiLogWriter.
    * Eliminate startable and common start logic from everything that is IStartable
    * Make flushing on a regular interval (eg .5s) work.

---
+++ src/LogJam/LogJamSetupException.cs
@@ -0,0 +1,31 @@
+﻿// // --------------------------------------------------------------------------------------------------------------------
+// <copyright file="LogJamSetupException.cs">
+// Copyright (c) 2011-2015 logjam.codeplex.com.  
+// </copyright>
+// Licensed under the <a href="http://logjam.codeplex.com/license">Apache License, Version 2.0</a>;
+// you may not use this file except in compliance with the License.
+// --------------------------------------------------------------------------------------------------------------------
+
+
+namespace LogJam
+{
+	using System;
+
+
+	/// <summary>
+	/// Signifies an error in a setup operation.
+	/// </summary>
+	public class LogJamSetupException : LogJamException
+	{
+
+		internal LogJamSetupException(string message, object source)
+			: base(message, source)
+		{}
+
+		internal LogJamSetupException(string message, Exception innerException, object source)
+			: base(message, innerException, source)
+		{}
+
+	}
+
+}
