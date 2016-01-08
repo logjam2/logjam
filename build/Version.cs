ba888d685eda2cdf078f122a980a78dbf13e794e
Crim, John <john.crim@gmail.com>
2016-01-08 00:07:08 -0800

    Update version to 1.0.1

--- build/Version.cs
+++ build/Version.cs
@@ -38,4 +38,4 @@ using System.Reflection;
 
 // Semantic version (http://semver.org). Assembly version changes less than the semver; semver must increment for every release.
 
-[assembly: AssemblyInformationalVersion("1.0.0-beta")]
+[assembly: AssemblyInformationalVersion("1.0.1-beta")]
