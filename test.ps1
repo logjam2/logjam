a3fdb7f23add01a917a4866116646a546dafe683
John Crim <john.crim@gmail.com>
2017-10-09 11:57:42 -0700

    #21 Try using test.ps1 script in appveyor

--- test.ps1
+++ test.ps1
@@ -1,6 +1,6 @@
 $testProjects =  ( "LogJam.UnitTests", "LogJam.Internal.UnitTests", "LogJam.XUnit2.UnitTests" )
 foreach ( $testProject in $testProjects) {
   pushd ".\test\$testProject"
-  dotnet xunit -nobuild $testProject.csproj
+  dotnet xunit -nobuild $testProject.csproj -appveyor
   popd
 }
