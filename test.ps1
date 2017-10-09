6db97f979a23e0ac9f9b4a7674fcf0c984770e39
John Crim <john.crim@gmail.com>
2017-10-09 13:59:02 -0700

    Working on build/tests for appveyor

--- test.ps1
+++ test.ps1
@@ -1,6 +1,9 @@
+# Dump info for https://github.com/xunit/xunit/issues/1345#issuecomment-335274948
+ dir -r -fi *.deps.json | select FullName
+
 $testProjects =  ( "LogJam.UnitTests", "LogJam.Internal.UnitTests", "LogJam.XUnit2.UnitTests" )
 foreach ( $testProject in $testProjects) {
   pushd ".\test\$testProject"
-  dotnet xunit -nobuild -appveyor -internaldiagnostics $testProject.csproj
+  dotnet xunit -nobuild -internaldiagnostics $testProject.csproj
   popd
 }
