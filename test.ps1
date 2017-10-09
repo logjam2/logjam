532ea81dfc6838f3c3b3f26fc7f9f7eae645e966
John Crim <john.crim@gmail.com>
2017-10-09 14:16:55 -0700

    Add dump $env:Configuration for appveyor troubleshooting

--- test.ps1
+++ test.ps1
@@ -1,5 +1,7 @@
 # Dump info for https://github.com/xunit/xunit/issues/1345#issuecomment-335274948
  dir -r -fi *.deps.json | select FullName
+"Configuration: $env:Configuration" 
+" "
 
 $testProjects =  ( "LogJam.UnitTests", "LogJam.Internal.UnitTests", "LogJam.XUnit2.UnitTests" )
 foreach ( $testProject in $testProjects) {
