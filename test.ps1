8c96c3e021b11f246b844310e873f862b7c2a8d7
John Crim <john.crim@gmail.com>
2017-10-18 15:32:58 -0700

    Improve test build script - don't lose the starting dir if a test fails.

--- test.ps1
+++ test.ps1
@@ -2,28 +2,30 @@
 # Powershell build script to run all tests in test projects - used by appveyor.
 #------------------------------------------------------------------
 
-"Configuration: $env:Configuration" 
+"Configuration: $env:Configuration"
 " "
 
 # Helper function, throws when an external executable returns a non-zero exit code.
-function Exec([scriptblock]$cmd, [string]$errorMessage = "Error executing command: " + $cmd) { 
-  & $cmd 
+function Exec([scriptblock]$cmd, [string]$errorMessage = "Error executing command: " + $cmd) {
+  & $cmd
   if ($LastExitCode -ne 0) {
-    throw $errorMessage 
-  } 
+    throw $errorMessage
+  }
 }
 
 $testProjects =  ( "LogJam.UnitTests", "LogJam.Internal.UnitTests", "LogJam.XUnit2.UnitTests" )
 foreach ( $testProject in $testProjects) {
-  pushd ".\test\$testProject"
-  if ($env:Configuration)
-  {
-    exec { dotnet xunit -nobuild -configuration $env:Configuration $testProject.csproj }
+  Push-Location ".\test\$testProject"
+  try {
+      if ($env:Configuration) {
+          exec { dotnet xunit -nobuild -configuration $env:Configuration $testProject.csproj }
+      }
+      else {
+          exec { dotnet xunit -nobuild $testProject.csproj }
+      }
   }
-  else
-  {
-    exec { dotnet xunit -nobuild $testProject.csproj }
+  finally {
+      Pop-Location
   }
-  popd
 }
 
